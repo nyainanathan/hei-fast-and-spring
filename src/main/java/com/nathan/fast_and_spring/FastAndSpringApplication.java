5bf6ee3016e1ab942d49e5ced364fcf21b174b5f
Ny Aina Nathan A. <nyaina.nathan.1@gmail.com>
2026-03-11 11:28:04 +0300

    project init

---
+++ src/main/java/com/nathan/fast_and_spring/FastAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nathan.fast_and_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastAndSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nathan/fast_and_spring/FastAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.fast_and_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastAndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
