444139622ae6a51ce23bacc33ba6824b473a9237
Ny Aina Nathan A. <nyaina.nathan.1@gmail.com>
2026-03-11 12:56:12 +0300

    chore: post booking endpoint for creating new booking

--- src/main/java/com/nathan/fast_and_spring/Reservation.java
+++ src/main/java/com/nathan/fast_and_spring/Reservation.java
@@ -3,6 +3,8 @@ package com.nathan.fast_and_spring;
 import java.time.LocalDate;
 
 public class Reservation {
+
+    private Integer id;
     private String clientName;
     private String clientPhoneNumber;
     private String clientEmail;
@@ -19,13 +21,126 @@ public class Reservation {
         this.roomDescription = roomDescription;
         this.reservationDate = reservationDate;
     }
+
     
-    public Integer getRoomNumber() {
-        return this.roomNumber;
+    public void SetEverything(Reservation newRes){
+        if(newRes.getClientName() != null)
+            this.setClientEmail(newRes.getClientEmail());
+        
+        if(newRes.getClientName() != null)
+            this.setClientName(newRes.getClientName());
+        
+        if(newRes.getClientPhoneNumber() != null)
+            this.setClientPhoneNumber(newRes.getClientPhoneNumber());;
+        
+        if(newRes.getReservationDate() != null)
+            this.setReservationDate(newRes.getReservationDate());
+        
+        if(newRes.getRoomDescription()!=null)
+            this.setRoomDescription(newRes.getRoomDescription());
+        
+        if(newRes.getRoomNumber() != null)
+            this.setRoomNumber(newRes.getRoomNumber());
     }
 
+
+
+    /**
+     * @return Integer return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return String return the clientName
+     */
+    public String getClientName() {
+        return clientName;
+    }
+
+    /**
+     * @param clientName the clientName to set
+     */
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    /**
+     * @return String return the clientPhoneNumber
+     */
+    public String getClientPhoneNumber() {
+        return clientPhoneNumber;
+    }
+
+    /**
+     * @param clientPhoneNumber the clientPhoneNumber to set
+     */
+    public void setClientPhoneNumber(String clientPhoneNumber) {
+        this.clientPhoneNumber = clientPhoneNumber;
+    }
+
+    /**
+     * @return String return the clientEmail
+     */
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    /**
+     * @param clientEmail the clientEmail to set
+     */
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    /**
+     * @return Integer return the roomNumber
+     */
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    /**
+     * @param roomNumber the roomNumber to set
+     */
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    /**
+     * @return String return the roomDescription
+     */
+    public String getRoomDescription() {
+        return roomDescription;
+    }
+
+    /**
+     * @param roomDescription the roomDescription to set
+     */
+    public void setRoomDescription(String roomDescription) {
+        this.roomDescription = roomDescription;
+    }
+
+    /**
+     * @return LocalDate return the reservationDate
+     */
     public LocalDate getReservationDate() {
-        return this.reservationDate;
+        return reservationDate;
+    }
+
+    /**
+     * @param reservationDate the reservationDate to set
+     */
+    public void setReservationDate(LocalDate reservationDate) {
+        this.reservationDate = reservationDate;
     }
 
 }
--- src/main/java/com/nathan/fast_and_spring/ReservationController.java
+++ src/main/java/com/nathan/fast_and_spring/ReservationController.java
@@ -3,16 +3,47 @@ package com.nathan.fast_and_spring;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/booking")
 public class ReservationController {
     
     private List<Reservation> reservations;
 
-    @GetMapping("/booking")
+    @GetMapping
     public List<Reservation> getReservations(){
         return this.reservations;
     }
 
+    @PostMapping
+    public List<Reservation> saveOrUpdatee(List<Reservation> toSave){
+
+        for(Reservation newRes : toSave){
+
+            //Verify if the booking is already present
+            int reservationIndex = -1;
+
+            for(int i = 0 ; i < reservations.size() ; i++){
+                if(newRes.getId() == reservations.get(i).getId()){
+                    reservationIndex = i;
+                    break;
+                }
+            }
+
+            //By this time
+            //If reservationIndex is still 0, then update
+            //Otherwise create a new Reservation
+
+            if(reservationIndex == -1) {
+                reservations.add(newRes);
+            } else {
+                reservations.get(reservationIndex).SetEverything(newRes);
+            }
+        }
+
+        return toSave;
+    }
 }
